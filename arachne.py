7688d2049cba7835bab34b4c3fe94eda982438c3
Chris Pickel <sfiera@sfzmail.com>
2013-11-12 21:26:36 +0900

    Fetch addons too.

--- arachne.py
+++ arachne.py
@@ -4,36 +4,30 @@ import bs4
 import collections
 import itertools
 import os
+import sys
 import utils
+import urlparse
 
 
-def is_last_page(url, doc):
-    this_kind, this_n, this_page = utils.url_to_page(url)
-    for link in doc.find_all("a"):
-        href = link.get("href")
-        if not href:
-            continue
-        that_kind, that_n, that_page = utils.url_to_page(href)
-        if (this_kind, this_n) != (that_kind, that_n):
-            continue
-        if that_page > this_page:
-            return False
-    return True
+def fix_links(url, doc):
+    for tag in doc.find_all("base"):
+        url = tag["href"]
+        tag.extract()
 
-
-def fix_links(doc):
-    links = doc.find_all("a")
+    links = doc.find_all("a") + doc.find_all("link")
     for link in links:
         href = link.get("href")
         if not href:
             continue
         kind, n, page = utils.url_to_page(href)
         if kind is not None:
-            link["href"] = "../../%s/%d/%d.html" % (kind, n, page)
+            link["href"] = "../../%s" % utils.local_path((kind, n, page))
+        else:
+            link["href"] = urlparse.urljoin(url, link["href"])
 
 
 def save(url, doc):
-    path = "out/%s/%d/%d.html" % utils.url_to_page(url)
+    path = "out/%s" % utils.local_path(utils.url_to_page(url))
     try:
         os.makedirs(os.path.dirname(path))
     except OSError:
@@ -53,10 +47,21 @@ def all_topic_urls(doc):
             yield n, page
 
 
+def all_addon_ids(doc):
+    links = doc.find_all("a")
+    for link in links:
+        href = link.get("href")
+        if not href:
+            continue
+        kind, n, page = utils.url_to_page(href)
+        if kind == "addon":
+            yield n
+
+
 def should_fix_links(url):
     page = utils.url_to_page(url)
     old = utils.cache_url(page)
-    new = "out/%s/%d/%d.html" % page
+    new = "out/%s" % utils.local_path(page)
     try:
         st_old = os.stat(old)
         st_new = os.stat(new)
@@ -69,31 +74,69 @@ def all_forum_pages(forum):
     for i in itertools.count(1):
         url = utils.page_to_url("forum", forum, i)
         doc = bs4.BeautifulSoup(utils.fetch(url))
-        last = is_last_page(url, doc)
+        last = utils.is_last_page(url, doc)
+        yield i, url, doc
+        if last:
+            break
+
+
+def all_addon_pages(game):
+    for i in itertools.count(1):
+        url = utils.page_to_url("addons", game, i)
+        doc = bs4.BeautifulSoup(utils.fetch(url))
+        last = utils.is_last_page(url, doc)
         yield i, url, doc
         if last:
             break
 
 
 def main():
-    for forum in [17, 64, 90]:
-        topics = collections.defaultdict(lambda: 1)
+    assert sys.argv[2:]
+    kind, n = sys.argv[1], sys.argv[2:]
 
-        for i, url, doc in all_forum_pages(forum):
-            for n, page in all_topic_urls(doc):
-                topics[n] = max(topics[n], page)
-            if should_fix_links(url):
-                fix_links(doc)
-                save(url, doc)
+    if kind == "forum":
+        for forum in n:
+            topics = collections.defaultdict(lambda: 1)
 
-        for topic, page in sorted(topics.iteritems()):
-            for i in xrange(1, page + 1):
-                url = utils.page_to_url("topic", topic, i)
-                data = utils.fetch(url)
+            for i, url, doc in all_forum_pages(forum):
+                for n, page in all_topic_urls(doc):
+                    topics[n] = max(topics[n], page)
                 if should_fix_links(url):
-                    doc = bs4.BeautifulSoup(data)
-                    fix_links(doc)
+                    fix_links(url, doc)
                     save(url, doc)
+
+            for topic, page in sorted(topics.iteritems()):
+                for i in xrange(1, page + 1):
+                    url = utils.page_to_url("topic", topic, i)
+                    data = utils.fetch(url)
+                    if should_fix_links(url):
+                        doc = bs4.BeautifulSoup(data)
+                        fix_links(url, doc)
+                        save(url, doc)
+    elif kind == "addons":
+        for game in n:
+            addons = []
+            for i, url, doc in all_addon_pages(game):
+                if should_fix_links(url):
+                    fix_links(url, doc)
+                    save(url, doc)
+                for n in all_addon_ids(doc):
+                    addons.append(n)
+            for n in sorted(addons):
+                url = utils.page_to_url("addon", n, None)
+                _ = utils.fetch(url)
+                path = utils.local_path(utils.url_to_page(url))
+                try:
+                    os.unlink("out/%s" % path)
+                except OSError:
+                    pass
+                try:
+                    os.makedirs(os.path.dirname("out/%s" % path))
+                except OSError:
+                    pass
+                os.link("cache/%s" % path, "out/%s" % path)
+    else:
+        assert False
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -2,15 +2,18 @@
 
 import datetime
 import os
+import re
 import sys
 import time
 import urllib2
 import urlparse
 
-CACHE_SECONDS = datetime.timedelta(days=14).total_seconds()
+CACHE_SECONDS = datetime.timedelta(days=30).total_seconds()
 URLS = {
-    "forum": u"http://www.ambrosiasw.com/forums/index.php?showforum=%d&st=%d&prune_day=100",
-    "topic": u"http://www.ambrosiasw.com/forums/index.php?showtopic=%d&st=%d",
+    "forum": u"http://www.ambrosiasw.com/forums/index.php?showforum=%s&st=%s&prune_day=100",
+    "topic": u"http://www.ambrosiasw.com/forums/index.php?showtopic=%s&st=%s",
+    "addons": u"http://www.ambrosiasw.com/games/%s/addons?page=%s",
+    "addon": u"http://www.ambrosiasw.com/assets/modules/addonfiles/download.php?addon=%s",
 }
 
 
@@ -19,32 +22,46 @@ def page_to_url(kind, n, page=1):
         st = (page - 1) * 25
     elif kind == "forum":
         st = (page - 1) * 30
-    else:
+    elif kind == "addons":
         st = page
+    else:
+        return URLS[kind] % n
     return URLS[kind] % (n, st)
 
 
 def url_to_page(url):
     url = urlparse.urlparse(url)
     qs = urlparse.parse_qs(url.query)
-    st = int(qs.get("st", [0])[-1])
-    page = int(qs.get("page", [0])[-1])
 
-    if url.path.endswith("/index.php"):
+    if url.netloc and not url.netloc.endswith("ambrosiasw.com"):
+        return None, None, None
+    if url.path == "/forums/index.php":
+        st = int(qs.get("st", [0])[-1])
         if "showtopic" in qs:
             return "topic", int(qs["showtopic"][-1]), 1 + (st / 25)
         elif "showuser" in qs:
             return "user", int(qs["showuser"][-1]), 1
         else:
             return "forum", int(qs.get("showforum", [0])[-1]), 1 + (st / 30)
-    elif url.path.endswith("/addons"):
-        game = url.split("/")[-2]
+    elif re.match("^/games/.*/addons/?$", url.path):
+        game = url.path.split("/")[2]
+        page = int(qs.get("page", [0])[-1])
         return "addons", game, page
+    elif url.path == "/assets/modules/addonfiles/download.php":
+        return "addon", int(qs["addon"][-1]), 1
     return None, None, None
 
 
+def local_path(page):
+    kind, n, st = page
+    if kind == "addon":
+        return "addon/%s.bin" % n
+    else:
+        return "%s/%s/%s.html" % page
+
+
 def cache_url(page):
-    return "cache/%s/%s/%s.html" % page
+    return "cache/%s" % local_path(page)
 
 
 def fetch(url):
@@ -82,3 +99,15 @@ def fetch(url):
     return data
 
 
+def is_last_page(url, doc):
+    this_kind, this_n, this_page = url_to_page(url)
+    for link in doc.find_all("a"):
+        href = link.get("href")
+        if not href:
+            continue
+        that_kind, that_n, that_page = url_to_page(href)
+        if (this_kind, this_n) != (that_kind, that_n):
+            continue
+        if that_page > this_page:
+            return False
+    return True
